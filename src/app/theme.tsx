0adc29b3fa08c453526e84088642fb3e75160191
shingalahitesh <shingalahitesh2@gmail.com>
2023-08-31 11:11:56 +0530

    - added MUI.  - added themes for Dark Mode.  - update radio button with MUI code.

---
+++ src/app/theme.tsx
@@ -0,0 +1,75 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+// Create a light theme
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  components: {
+    MuiRadio: {
+      styleOverrides: {
+        root: {
+          '&.Mui-checked': {
+            color: '#1976d2', // Color when checked in light mode
+          },
+        },
+      },
+    },
+  }
+});
+
+// Create a dark theme by extending the light theme and overriding specific values
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#ff4081',
+    },
+  },
+  components: {
+    MuiRadio: {
+      styleOverrides: {
+        root: {
+          '&.Mui-checked': {
+            color: '#90caf9', // Color when checked in light mode
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          background: '#333',
+          color: '#fff',
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        h1: {
+          color: '#fff',
+        },
+      },
+    },
+  },
+});
+
+
+export { lightTheme, darkTheme };
