508c438acb0feec003716c57df9126eea74da532
shingalahitesh <shingalahitesh2@gmail.com>
2023-09-05 16:35:33 +0530

    - added filler functionality on common auto search component.

--- src/components/common/selectWithSearch.tsx
+++ src/components/common/selectWithSearch.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { BsSearch} from "react-icons/bs";
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 export const  SelectWithSearch = (props: any) => {
+  const [inputValue, setInputValue] = useState('');
   const { options, label, value, onChange, placeholder } = props;
+  const filterOptions = (options: any[], { inputValue }: {inputValue: string}) => {
+    return options.filter(
+      (option) =>{
+        if(option.label && option.value) {
+          return option.label.toLowerCase().includes(inputValue.toLowerCase()) ||
+          option.value.toLowerCase().includes(inputValue.toLowerCase())
+        } else {
+          return true;
+        }
+      }
+    );
+  };
+
+
   return (
     <Autocomplete
       value={value}
       onChange={(_, newValue) => onChange(newValue)}
       options={options}
       autoComplete
+      filterOptions={filterOptions}
       includeInputInList
       id="auto-complete"
-      isOptionEqualToValue={(option, value) =>
-        option && label && option.label === value.label
-      }
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
       getOptionLabel={(option) => option.label}
       renderInput={(params) => (
         <>
