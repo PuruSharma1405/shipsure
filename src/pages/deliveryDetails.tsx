9a2a54715cafceaffe874c2d29a22d34ea9fb703
shingalahitesh <shingalahitesh2@gmail.com>
2023-09-07 16:04:06 +0530

    - added Option interface.  - update delivery details and order details for add interfaces.  - update delivery details UI for responsive.  - added order title on redux.  -

--- src/pages/deliveryDetails.tsx
+++ src/pages/deliveryDetails.tsx
@@ -3,16 +3,12 @@ import React, { useState, useEffect } from "react";
 import {
   AiOutlineSearch,
   AiOutlineClose,
-  AiOutlineShoppingCart,
-  AiOutlineArrowRight
 } from "react-icons/ai";
 import { CgMenuGridO } from "react-icons/cg";
 import ProfileDropDown from "@/components/createRequisition/ProfileDropDown";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import RequisitionDataContainer from "../components/createRequisition/RequisitionDataContainer";
 import { useSelector, useDispatch } from "react-redux";
 import HorizontalLinearStepper from '../components/createRequisitionSpares/Stepper'
-import { Container } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { setDeliveryDetailsState, selectDeliveryDetailsState } from "@/redux/reducers/deliveryDetailsSlice";
@@ -24,28 +20,43 @@ import FormControl from '@mui/material/FormControl';
 import { getToken } from '@/services/localstorageService';
 import { getHomeList, getOtherPortList, getPositionList } from '@/services/operations/deliveryDetailsApi';
 import { SelectBox } from '@/components/common/SelectBox';
-import { SearchWithDropDown } from '@/components/common/SearchWithDropdown';
 import { MultiLineTextBox } from '@/components/common/multiLineTextBox';
 import { SelectWithSearch } from '@/components/common/selectWithSearch';
 import { selectRequisitionState, setDeliveryDetails } from "@/redux/reducers/requisitionSlice";
 import { useRouter } from 'next/router';
+import Link from "next/link";
+import { IOption } from '@/interfaces/index';
+import Layout from '@/components/common/requisitionLayout';
+import { Button, Grid, FormHelperText } from '@mui/material';
 
 const DeliveryDetails = () => {
-  const itemValue = localStorage.getItem("itemName");
   const router = useRouter();
   const dispatch = useDispatch();
   const requisitionStateData = useSelector(selectRequisitionState);
   const [item, setItem] = useState("position_list");
   const [homePortOptions, setHomePortOptions] = useState([]);
-  const [selectedHomePort, setSelectedHomePort] = useState(null);
+  const [selectedHomePort, setSelectedHomePort] = useState<IOption>({});
+  const [selectedHomePortError, setSelectedHomePortError] = useState('');
 
   const [otherPortOptions, setOtherPortOptions] = useState([]);
-  const [selectedOtherPort, setSelectedOtherPort] = useState(null);
+  const [selectedOtherPort, setSelectedOtherPort] = useState<IOption>({});
+  const [selectedOtherPortError, setSelectedOtherPortError] = useState('');
 
   const [positionListOptions, setPositionListOptions] = useState([]);
-  const [selectedPositionList, setSelectedPositionList] = useState(null);
-  
+  const [selectedPositionList, setSelectedPositionList] = useState<IOption>({});
+  const [selectedPositionListError, setSelectedPositionListError] = useState('');
+
+  const itemName = localStorage.getItem('itemName')
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedDateError, setSelectedDateError] = useState('');
+
+
+  // for error handling
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  useEffect(() => {
+    setIsFormSubmitted(false);
+  }, [selectedHomePort, selectedOtherPort, selectedPositionList, selectedDate])
 
   useEffect(() => {
     fetchData();
@@ -58,15 +69,15 @@ const DeliveryDetails = () => {
   const fetchData = async () => {
     const token = await getToken();
     let homeListParams: any = {};
-    if(requisitionStateData.vesId) {
+    if (requisitionStateData.vesId) {
       homeListParams['VesId'] = requisitionStateData.vesId
     }
-    if(requisitionStateData.coyId) {
+    if (requisitionStateData.coyId) {
       homeListParams['CoyId'] = requisitionStateData.coyId
     }
     const homePortRes = await getHomeList(token, homeListParams);
     const { recordset } = homePortRes;
-    if(recordset) {
+    if (recordset) {
       const homePortOptions = recordset.map((el: any) => {
         return {
           label: el.PrtIdDefHome,
@@ -77,7 +88,7 @@ const DeliveryDetails = () => {
     }
 
     const otherPortRes = await getOtherPortList(token, {});
-    if(otherPortRes?.result?.recordset) {
+    if (otherPortRes?.result?.recordset) {
       const otherPortOptions = otherPortRes.result.recordset.map((el: any) => {
         return {
           value: el.PrtId,
@@ -88,31 +99,72 @@ const DeliveryDetails = () => {
     }
 
     let positionListParams: any = {};
-    if(requisitionStateData.vesId) {
+    if (requisitionStateData.vesId) {
       positionListParams.VesselId = requisitionStateData.vesId
     }
-    if(selectedDate) {
+    if (selectedDate) {
       positionListParams.ExpectedDeliveryDate = selectedDate
     }
     const positionList = await getPositionList(token, positionListParams);
-    if(positionList?.recordset) {
-      const otherPortOptions = positionList.recordset.map((el: any) => {
+    if (positionList?.recordset) {
+      const positionListOptions = positionList.recordset.map((el: any) => {
         return {
           label: el.PrtName,
           value: el.PrtIdDefHome
         }
       });
-      setPositionListOptions(otherPortOptions);
+      if(positionListOptions && positionListOptions.length > 0) {
+        setPositionListOptions(positionListOptions);
+      }
     }
   }
-  
+
 
   const [notes, setNotes] = useState('');
   const updateNotes = (e: any) => {
     setNotes(e.target.value);
   }
+  const validateData = (): boolean => {
+    let isValid = true;
+    setIsFormSubmitted(true);
+
+    if (!selectedDate) {
+      setSelectedDateError('Please Select Date');
+      isValid = false;
+    } else {
+      setSelectedDateError('');
+    }
+
+    if (item === 'position_list' && (!selectedPositionList || !selectedPositionList.value || !selectedPositionList.value.trim())) {
+      setSelectedPositionListError('Please Position');
+      isValid = false;
+    } else {
+      setSelectedPositionListError('');
+    }
+
+    if (item === 'home_port' && (!selectedHomePort || !selectedHomePort.value || !selectedHomePort.value.trim())) {
+      setSelectedHomePortError('Please Position');
+      isValid = false;
+    } else {
+      setSelectedHomePortError('');
+    }
+
+    if (item === 'other_port' && (!selectedOtherPort || !selectedOtherPort.value || !selectedOtherPort.value.trim())) {
+      setSelectedOtherPortError('Please Position');
+      isValid = false;
+    } else {
+      setSelectedOtherPortError('');
+    }
+
+    return isValid;
+
+  }
 
   const handleNext = () => {
+    if (!validateData()) {
+      return;
+    }
+
     dispatch(setDeliveryDetails({
       deliveryDate: selectedDate,
       deliveryHomePort: selectedHomePort,
@@ -121,17 +173,10 @@ const DeliveryDetails = () => {
       deliveryLocation: item,
       note: notes,
     }));
-    setTimeout(()=> {
+    setTimeout(() => {
       router.push('/orderSummary');
     }, 100)
   }
-  const updatePositionList = (e: any) => {
-    setSelectedPositionList(e.target.value);
-  }
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-  };
 
 
   const handleDateChange = (date: Date | null) => {
@@ -142,117 +187,140 @@ const DeliveryDetails = () => {
     setItem(e.target.value);
   };
 
+  const handlePrevious = () => {
+    router.push('/orderDetails');
+  }
+
   return (
-    <div className="h-[100vh]  relative w-[100vw] bg-[#FFFFFF] overflow-x-hidden overflow-y-auto">
-      <div className="mx-auto">
-        <div className="flex justify-between w-11/12 items-center mx-auto">
-          <div className="text-2xl font-bold mt-3">
-            <h2>Procurement</h2>
-          </div>
-          <div className="search-icon mt-3 gap-3 flex items-center">
-            <AiOutlineSearch style={{ fontSize: "25px" }} />
-            <IoMdNotificationsOutline style={{ fontSize: "25px" }} />
-            <CgMenuGridO style={{ fontSize: "25px" }} />
-            <ProfileDropDown />
-          </div>
+    <Layout>
+      <div className="flex justify-between w-10/12 items-center mx-auto">
+        <div className="text-2xl font-bold mt-3">
+          <h2>Procurement</h2>
         </div>
-        <div className="h-full w-full flex flex-row">
-          <div className=" w-8/12 mt-7 ml-36 relative">
-            <div className="flex flex-row justify-between">
-              <h1 className="text-2xl font-semibold">
-                Create Requisition - {itemValue}
-              </h1>
-              <AiOutlineClose style={{ fontSize: "25px" }} />
-            </div>
-            <HorizontalLinearStepper />
-              <div className='delivery-details'>
-                <Container>
-                  <Card variant="outlined" sx={{ maxWidth: '60%' }}>
-                    <CardContent>
-                      <form onSubmit={handleSubmit}>
-                        <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-                          <CustomDatePicker label="select Date" value={selectedDate} onChange={handleDateChange} />
-                        </FormControl>
-                        <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-                          <RadioGroup
-                            row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
-                          >
-                            <FormControlLabel value="position_list" control={<Radio checked={item === 'position_list'} onChange={itemChange} />} label="Position List" />
-                            <FormControlLabel value="home_port" control={<Radio checked={item === 'home_port'} onChange={itemChange} />} label="Home Port" />
-                            <FormControlLabel value="other_port" control={<Radio checked={item === 'other_port'} onChange={itemChange} />} label="Other Port" />
-                          </RadioGroup>
-                        </FormControl>
-                        <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-                          {item === 'position_list' ? (
-                            <SelectBox options={positionListOptions} value={selectedPositionList} label="Position List" onChange={updatePositionList}/>
-                          ) : item === 'home_port' ? 
-                           <SelectWithSearch 
-                           label='select port'
-                           options= {homePortOptions}
-                           onChange= {setSelectedHomePort}
-                           value={selectedHomePort}
-                           placeholder="Select Delivery Port"
-                         />
-                          : 
-                          <SelectWithSearch 
-                            label='select port'
-                            options= {otherPortOptions}
-                            onChange= {setSelectedOtherPort}
-                            value={selectedOtherPort}
-                            placeholder="Select Delivery Port"
-                          />
-                          }
-                        </FormControl>
-
-
-                        <FormControl fullWidth sx={{ m: 1 }} variant="filled" className="notes">
-                          <MultiLineTextBox 
-                            label="Notes"
-                            value={notes}
-                            onChange={updateNotes}
-                          />
-                          
-                        </FormControl>
-                      </form>
-
-                    </CardContent>
-                  </Card>
-                </Container>
-              {/* </div> */}
-            </div>
-            <div onClick={handleNext} className="flex flex-row uppercase justify-center items-center p-2 w-[106px] text-center rounded-full font-bold text-white bg-[#11110E] absolute -bottom-14 right-0 hover:scale-95 transition-all duration-200">
-              <p className="text-[14px]">Next</p>
-              <AiOutlineArrowRight className="ml-1" />
-            </div>
-          </div>
-          <div className="w-4/12 bg-[#E8ECED] ml-[50px] h-[100vh]">
-            <div className="flex flex-row flex-wrap mt-[60px] ml-[50px]">
-              <RequisitionDataContainer basketValues="" vesselBasicDetails=""/>
-              <div
-                className="flex flex-col mt-5 bg-white shadow-lg rounded-lg  w-[350px] h-[100px]"
-                style={{ borderRadius: "15px" }}
-              >
-                <div className="flex flex-row  justify-between items-center m-5">
-                  <div className="flex flex-row ">
-                    <AiOutlineShoppingCart style={{ fontSize: "25px" }} />
-                    <h2 className="uppercase font-semibold ml-1">
-                      Order Basket
-                    </h2>
-                  </div>
-                  <p className="font-bold">$0.00</p>
-                </div>
-                <div className="flex flex-row justify-between ml-10">
-                  <p className="font-semibold">0 items</p>
-                  <h3 className="relative right-4 font-bold">estimated</h3>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="search-icon mt-3 gap-3 flex items-center">
+          <AiOutlineSearch style={{ fontSize: "25px" }} />
+          <IoMdNotificationsOutline style={{ fontSize: "25px" }} />
+          <CgMenuGridO style={{ fontSize: "25px" }} />
+          <ProfileDropDown />
         </div>
       </div>
-    </div>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-2xl font-semibold">
+          Create Requisition - {itemName}
+        </h1>
+        <Link href="/createRequisition"><AiOutlineClose style={{ fontSize: "25px" }} /></Link>
+      </div>
+
+      <HorizontalLinearStepper />
+      <div className="flex justify-center margin-top-20-px">
+        <Card variant="outlined" sx={{
+          backgroundColor: '#F5F5F5',
+          width: {
+            xs: '90%',
+            sm: '65%',
+            md: '54%',
+            lg: '45%',
+          }
+        }}>
+          <CardContent sx={{ padding: '2em' }}>
+            <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+              <CustomDatePicker label="select Date" value={selectedDate} onChange={handleDateChange} />
+            </FormControl>
+            <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel value="position_list" control={<Radio checked={item === 'position_list'} onChange={itemChange} />} label="Position List" />
+                <FormControlLabel value="home_port" control={<Radio checked={item === 'home_port'} onChange={itemChange} />} label="Home Port" />
+                <FormControlLabel value="other_port" control={<Radio checked={item === 'other_port'} onChange={itemChange} />} label="Other Port" />
+              </RadioGroup>
+            </FormControl>
+            {item === 'position_list' ? (
+              <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+                <SelectWithSearch
+                  label='Position List'
+                  options={positionListOptions}
+                  onChange={setSelectedPositionList}
+                  value={selectedPositionList}
+                  placeholder="Position List"
+                />
+                {(selectedPositionListError && isFormSubmitted) &&
+                  <FormHelperText error={true}>{selectedPositionListError}</FormHelperText>
+                }
+              </FormControl>
+
+            ) : item === 'home_port' ?
+              <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+
+                <SelectWithSearch
+                  label='select port'
+                  options={homePortOptions}
+                  onChange={setSelectedHomePort}
+                  value={selectedHomePort}
+                  placeholder="Select Delivery Port"
+                />
+                {(selectedHomePortError && isFormSubmitted) &&
+                  <FormHelperText error={true}>{selectedHomePortError}</FormHelperText>
+                }
+              </FormControl>
+              :
+              <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+
+                <SelectWithSearch
+                  label='select port'
+                  options={otherPortOptions}
+                  onChange={setSelectedOtherPort}
+                  value={selectedOtherPort}
+                  placeholder="Select Delivery Port"
+                />
+                {(selectedHomePortError && isFormSubmitted) &&
+                  <FormHelperText error={true}>{selectedHomePortError}</FormHelperText>
+                }
+              </FormControl>
+            }
+
+
+            <FormControl fullWidth sx={{ m: 1 }} variant="filled" className="notes">
+              <MultiLineTextBox
+                label="Notes"
+                value={notes}
+                onChange={updateNotes}
+              />
+
+            </FormControl>
+          </CardContent>
+        </Card>
+      </div>
+      <Grid container spacing={2} alignItems="center" justifyContent="center" className="margin-top-20-px">
+        <Grid item xs={12} sm={8} md={6}>
+          <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+            <Grid item xs={4} sm={3} md={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handlePrevious}
+                fullWidth
+              >
+                Previous
+              </Button>
+            </Grid>
+            <Grid item xs={4} sm={3} md={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+                fullWidth
+              >
+                Next
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+    </Layout>
   );
 };
 
